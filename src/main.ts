b0e47c7c1db8edd7e04baf09b563ba341c757ad5
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-21 19:08:46 +0200

    Add ElIcon and ElNotification globally

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App).use(store).use(router);
+
+// Element Plus
+import { ElIcon, ElNotification } from "element-plus";
+const components = [ElIcon];
+const plugins = [ElNotification];
+
+components.forEach((component) => app.use(component));
+plugins.forEach((plugin) => app.use(plugin));
+
+app.mount("#app");

4fe2a8e608df6c6a26f078560c5cbad29ecca75a
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-21 19:08:17 +0200

    Use scss styles for element-plus

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = {
       {
         libraryName: "element-plus",
         customStyleName: (name) => {
-          return `element-plus/lib/theme-chalk/${name}.css`;
+          name = name.slice(3);
+          return `element-plus/packages/theme-chalk/src/${name}.scss`;
         },
       },
     ],

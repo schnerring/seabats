351c060c6c80d04e7663b3fdefcca15c74808415
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-21 02:30:47 +0200

    Add LICENSE and attributions

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+
+    <h2>Attribution</h2>
+
+    <p>
+      Aviation data is provided by
+      <a class="opensky-attribution" href="http://www.opensky-network.org"
+        >The OpenSky Network, http://www.opensky-network.org</a
+      >
+    </p>
+
+    <p class="opensky-attribution">
+      Bringing up OpenSky: A large-scale ADS-B sensor network for research
+      Matthias Schäfer, Martin Strohmeier, Vincent Lenders, Ivan Martinovic,
+      Matthias Wilhelm ACM/IEEE International Conference on Information
+      Processing in Sensor Networks, April 2014
+    </p>
+
+    <h2>License</h2>
+
+    <p>
+      The source code is
+      <a href="https://github.com/schnerring/seabats">available on GitHub</a>
+      and
+      <a href="https://github.com/schnerring/seabats/blob/main/LICENSE"
+        >licensed under MIT</a
+      >.
+    </p>
+
+    <p></p>
   </div>
 </template>
+
+<style scoped>
+.opensky-attribution {
+  font-style: italic;
+}
+</style>

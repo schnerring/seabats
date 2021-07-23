59b67fa426691b36ea841fdbdd3544c2ea2d8056
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-23 04:11:28 +0200

    Remove OpenSky attribution since it's no longer used

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,20 +4,6 @@
 
     <h2>Attribution</h2>
 
-    <p>
-      Aviation data is provided by
-      <a class="opensky-attribution" href="http://www.opensky-network.org"
-        >The OpenSky Network, http://www.opensky-network.org</a
-      >
-    </p>
-
-    <p class="opensky-attribution">
-      Bringing up OpenSky: A large-scale ADS-B sensor network for research
-      Matthias Schäfer, Martin Strohmeier, Vincent Lenders, Ivan Martinovic,
-      Matthias Wilhelm ACM/IEEE International Conference on Information
-      Processing in Sensor Networks, April 2014
-    </p>
-
     <h2>License</h2>
 
     <p>

1952f79bd90f061ae5c17d627ebc46b9b4f795e1
MrDussel <52812681+MrDussel@users.noreply.github.com>
2021-07-24 23:54:41 +0200

    Fix ADSB data service error handling

--- src/shared/adsb/SampleDataService.ts
+++ src/shared/adsb/SampleDataService.ts
@@ -7,37 +7,37 @@ import dayjs from "dayjs";
 
 export class SampleDataService extends HttpServiceBase {
   async getFlights(): Promise<Flight[]> {
-    const response = await this.instance.get<TraceFileResponse[]>(
-      "adsb/sample-traces.json"
-    );
-
-    if (!response.data)
+    try {
+      const response = await this.instance.get<TraceFileResponse[]>(
+        "adsb/sample-traces.json"
+      );
+      return response.data.map((tc) => {
+        return <Flight>{
+          icao: tc.icao ?? tc.hex,
+          date: dayjs.unix(tc.timestamp).toDate(),
+          traces: tc.trace.map((t) => {
+            const flags = <number>t[TraceFileFields.Flags];
+            return <Trace>{
+              date: dayjs
+                .unix(<number>t[TraceFileFields.SecondsAfterTimestamp])
+                .toDate(),
+              lat: t[TraceFileFields.Lat],
+              lon: t[TraceFileFields.Lon],
+              altitudeFt: t[TraceFileFields.AltitudeFt],
+              groundSpeedKts: t[TraceFileFields.GroundSpeedKts],
+              trackDeg: t[TraceFileFields.TrackDeg],
+              isStale: (flags & 1) > 0,
+              isLeg: (flags & 2) > 0,
+              isVerticalRateGeometric: (flags & 4) > 0,
+              isAltitudeGeometric: (flags & 8) > 0,
+              verticalRateFpm: t[TraceFileFields.VerticalRateFpm],
+              // TODO map the last field of aircraft info?
+            };
+          }),
+        };
+      });
+    } catch {
       throw "ADSB sample data not initialized. Please run the initalization script and try again.";
-
-    return response.data.map((tc) => {
-      return <Flight>{
-        icao: tc.icao ?? tc.hex,
-        date: dayjs.unix(tc.timestamp).toDate(),
-        traces: tc.trace.map((t) => {
-          const flags = <number>t[TraceFileFields.Flags];
-          return <Trace>{
-            date: dayjs
-              .unix(<number>t[TraceFileFields.SecondsAfterTimestamp])
-              .toDate(),
-            lat: t[TraceFileFields.Lat],
-            lon: t[TraceFileFields.Lon],
-            altitudeFt: t[TraceFileFields.AltitudeFt],
-            groundSpeedKts: t[TraceFileFields.GroundSpeedKts],
-            trackDeg: t[TraceFileFields.TrackDeg],
-            isStale: (flags & 1) > 0,
-            isLeg: (flags & 2) > 0,
-            isVerticalRateGeometric: (flags & 4) > 0,
-            isAltitudeGeometric: (flags & 8) > 0,
-            verticalRateFpm: t[TraceFileFields.VerticalRateFpm],
-            // TODO map the last field of aircraft info?
-          };
-        }),
-      };
-    });
+    }
   }
 }

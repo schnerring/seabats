ebce95e9e50a7f7e52f5c55c959626d8eb39ec57
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-30 00:11:12 +0200

    Implement xAxis

--- src/components/timeline/Timeline.vue
+++ src/components/timeline/Timeline.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="d3" ref="d3"></div>
+  <div class="d3" ref="d3">
+    <svg></svg>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import dayjs from "dayjs";
+import { debounce } from "lodash";
+import {
+  scaleTime,
+  axisBottom,
+  select,
+  Axis,
+  NumberValue,
+  scaleLinear,
+  ScaleLinear,
+  Selection,
+} from "d3";
 
 export default defineComponent({
   props: {
@@ -16,17 +29,60 @@ export default defineComponent({
       type: Date,
       default: dayjs().toDate(),
     },
+    height: {
+      type: Number,
+      default: 400,
+    },
   },
   data() {
     return {
       minDate: this.initMinDate,
       maxDate: this.initMaxDate,
-      resizeObserver: new ResizeObserver((entries) => {
-        for (const entry of entries) console.log(`Size change: ${entry}`);
-      }),
+      width: 0,
+      margin: { top: 10, right: 30, bottom: 30, left: 60 },
+      xAxisDefinition: {} as Axis<Date | NumberValue>,
+      xAxis: {} as Selection<SVGGElement, unknown, HTMLElement, any>,
+      svg: {} as Selection<SVGGElement, unknown, HTMLElement, any>,
+      xScale: {} as ScaleLinear<number, number, never>,
+      resizeObserver: new ResizeObserver(
+        debounce((entries) => {
+          this.update(entries);
+        }, 250)
+      ),
     };
   },
+  methods: {
+    update(entries: ResizeObserverEntry[]) {
+      entries.forEach((entry) => {
+        this.width = entry.contentBoxSize[0].inlineSize;
+        this.xScale.range([0, this.svgWidth()]);
+        this.xAxis.call(this.xAxisDefinition);
+        this.svg.transition().attr("width", this.svgWidth());
+      });
+    },
+    svgHeight() {
+      const height = this.height - this.margin.top - this.margin.bottom;
+      return height < 0 ? 0 : height;
+    },
+    svgWidth() {
+      const width = this.width - this.margin.left - this.margin.right;
+      return width < 0 ? 0 : width;
+    },
+  },
+  created() {
+    this.xScale = scaleLinear().domain([0, 100]).range([0, 100]);
+  },
   mounted() {
+    this.xAxisDefinition = axisBottom(this.xScale);
+    this.svg = select(".d3 svg");
+    this.svg.attr("width", this.svgWidth()).attr("height", this.svgHeight());
+
+    this.xAxis = this.svg
+      .append("g")
+      .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`)
+      .attr("class", "x-axis")
+      .call(this.xAxisDefinition);
+
     this.resizeObserver.observe(this.$refs["d3"] as Element);
   },
 });

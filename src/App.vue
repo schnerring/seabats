223c49ece65ea5f60a3e6b102c56473180603a75
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-07-30 04:57:34 +0200

    Add blue color palette

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,17 @@ body {
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
 :root {
-  --blau: rgb(64, 64, 247);
+  /* Material Blue */
+  --blue50: #e3f2fd;
+  --blue100: #bbdefb;
+  --blue200: #90caf9;
+  --blue300: #64b5f6;
+  --blue400: #42a5f5;
+  --blue500: #2196f3;
+  --blue600: #1e88e5;
+  --blue700: #1976d2;
+  --blue800: #1565c0;
+  --blue900: #0d47a1;
 }
 
 #app {

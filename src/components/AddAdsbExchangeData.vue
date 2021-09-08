a3b8db406c2b245070bb698b620763d969711990
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-09-08 18:40:05 +0200

    Change ADSB data upload JSON format

--- src/components/AddAdsbExchangeData.vue
+++ src/components/AddAdsbExchangeData.vue
@@ -41,9 +41,17 @@ export default defineComponent({
   methods: {
     async selectionChanged(files: FileList) {
       for (let i = 0; i < files.length; i++) {
-        const tc: AdsbExchangeResponse = JSON.parse(await files[i].text());
-        const flight = new Flight(tc);
-        await addFlight(flight);
+        const adsbTraces: AdsbExchangeResponse[] = JSON.parse(
+          await files[i].text()
+        );
+        for (const trace of adsbTraces) {
+          try {
+            const flight = new Flight(trace);
+            await addFlight(flight);
+          } catch {
+            console.warn(trace);
+          }
+        }
       }
     },
   },

d3d79e576f4f419c6436a03d751fa8b26ffac006
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-09-09 22:11:52 +0200

    Add info list and markdown component

---
+++ src/components/InfoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <markdown
+      v-for="flightInfo of flightInfos"
+      :key="flightInfo.flightId"
+      :markdown="flightInfo.markdown"
+    ></markdown>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
+import Markdown from "@/components/Markdown.vue";
+
+export default defineComponent({
+  components: {
+    Markdown,
+  },
+  computed: {
+    ...mapState(["flightInfos"]),
+  },
+  methods: {
+    ...mapActions(["getFlightInfos"]),
+  },
+  async created() {
+    await this.getFlightInfos();
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Markdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-html="html"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import marked from "marked";
+
+export default defineComponent({
+  props: {
+    markdown: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    html(): string {
+      return marked(this.markdown);
+    },
+  },
+});
+</script>
+
+<style scoped></style>

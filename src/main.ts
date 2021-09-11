84cf76816ee24f70dd1f005bdd8d8fd7768892dd
MrDussel <52812681+MrDussel@users.noreply.github.com>
2021-09-11 22:37:15 +0200

    Customize element-plus theme

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import { store } from "./store";
 
-const app = createApp(App).use(store).use(router);
-
 // Element Plus
 import { ElIcon, ElNotification } from "element-plus";
 const components = [ElIcon];
 const plugins = [ElNotification];
+
+import "@/assets/element-variables.scss";
+
+const app = createApp(App).use(store).use(router);
 
 components.forEach((component) => app.use(component));
 plugins.forEach((plugin) => app.use(plugin));

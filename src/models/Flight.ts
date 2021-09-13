2be42dc405bac06d2ce79ead6e7159fe829c87dc
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-09-13 10:10:01 +0200

    Add GeoJSON models

---
+++ src/models/Flight.ts
@@ -0,0 +1,7 @@
+export interface Flight {
+  id: string;
+  icao: string;
+  aircraft: string;
+  from: string;
+  to: string;
+}
---
+++ src/models/Zone.ts
@@ -0,0 +1,6 @@
+export interface Zone {
+  type: "12nm" | "24nm" | "sar";
+  title: string;
+  attribution: string;
+  color: string;
+}
